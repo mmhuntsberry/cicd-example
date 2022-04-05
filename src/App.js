4febf3c2cff34d54e826f2cca3f0acd8f37616de
matt.huntsberry <matt.huntsberry@nielsen.com>
2022-04-04 18:21:56 -0700

    chore: initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import '@nielsen-media/gds-button/lib/src';
+import '@nielsen-media/gds-menu/lib/src';
+import '@nielsen-media/gds-icons/lib/src/assets/add-user-outline';
 
 function App() {
   return (
@@ -9,13 +12,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* <gds-button text="Click me"></gds-button>
+        <gds-button text="Delete"></gds-button>
+        <gds-menu text="Label"></gds-menu> */}
+        <gds-icon-add-user-outline></gds-icon-add-user-outline>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn ACA
         </a>
       </header>
     </div>
